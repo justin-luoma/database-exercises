3d8476e3ccb812ad4e8022ab43e2c50344aca2fa
Justin Luoma <justin@justinluoma.com>
2018-09-27 11:34:41 -0500

    MySQL Tables: exercise completed

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(50) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  release_date INT(4) NOT NULL,
+  sales DECIMAL(5, 2) NOT NULL,
+  genre VARCHAR(25) NOT NULL,
+  PRIMARY KEY (id)
+)
